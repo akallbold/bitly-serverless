81e7a5064dfe555cc5b17bacb48151af3215cf93
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2023-05-25 19:01:12 -0700

    removed header

--- netlify/functions/createShortUrl.js
+++ netlify/functions/createShortUrl.js
@@ -3,8 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 const headers = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "Content-Type",
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE,OPTION",
+  "Access-Control-Allow-Methods": "POST, OPTION",
 };
 const handler = async (event) => {
   if (event.body) {
