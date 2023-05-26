56d27b9b46d0f13a74e3af16c32c3607595dd7fa
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2023-05-25 20:12:27 -0700

    updated functions

---
+++ netlify/functions/getLongUrl.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient();
+
+const handler = async (event) => {
+  if(event.body) {
+    const data = JSON.parse(event.body) 
+    const siteData = await prisma.sites.findFirst({
+      where: {
+        id: data.id,
+      },
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(siteData)
+    };
+  }
+
+  return {
+    statusCode: 500
+  };
+}
+
+
+export { handler }
+
--- netlify/functions/hello-world.js
+++
@@ -1,8 +0,0 @@
-const handler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: `Hello World` }),
-  };
-};
-
-export { handler };
