b36beb9f1461f09840077fac13514f81f093d52e
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2023-05-25 20:26:05 -0700

    changed from ts to js

--- netlify/functions/getLongUrl.js
+++ netlify/functions/getLongUrl.js
@@ -1,10 +1,10 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 const handler = async (event) => {
-  if(event.body) {
-    const data = JSON.parse(event.body) 
+  if (event.body) {
+    const data = JSON.parse(event.body);
     const siteData = await prisma.sites.findFirst({
       where: {
         id: data.id,
@@ -13,15 +13,13 @@ const handler = async (event) => {
 
     return {
       statusCode: 200,
-      body: JSON.stringify(siteData)
+      body: JSON.stringify(siteData),
     };
   }
 
   return {
-    statusCode: 500
+    statusCode: 500,
   };
-}
+};
 
-
-export { handler }
-
+export { handler };
