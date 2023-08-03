2e327edef7d436edff0294e4a9bc9121d3b72908
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2023-08-03 15:49:36 -0700

    update

--- netlify/functions/getLongUrl.js
+++ netlify/functions/getLongUrl.js
@@ -2,9 +2,11 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 const headers = {
-  "Access-Control-Allow-Origin": "*",
+  // "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Origin": "https://shantellsansnotes.com/*",
   "Access-Control-Allow-Methods": "POST, OPTION",
 };
+
 const handler = async (event) => {
   if (event.body) {
     const data = JSON.parse(event.body);
